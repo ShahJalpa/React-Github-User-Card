30b3c084654c1b60c4d97d77ca83443fc7a6cf49
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-02-09 17:34:33 -0500

    almost done with mvp

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      user: {},
+      followers: [],
+      following: []
+    }
+  };
+
+  componentDidMount(){
+    axios
+      .get("https://api.github.com/users/ShahJalpa")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          user: response.data
+        });
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+    axios
+      .get("https://api.github.com/users/ShahJalpa/followers")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          followers: response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })  
+
+    axios
+      .get("https://api.github.com/users/ShahJalpa/following")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          followers: response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+    })  
+  }
+  render(){
+    return (
+      <div className="App">
+        <div>
+          <h1>GitHub User Card</h1>
+        </div>
+        <div className="profileCardMaker">
+          <img src={this.state.user.avatar_url} alt={this.state.user.login} />
+          <h2>{this.state.user.name}</h2>
+          <p>{this.state.user.login}</p>
+          <p>{this.state.user.location}</p>
+          <a href={this.state.user.html_url} ></a>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
